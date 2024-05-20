bf3171586512f5ff41a9f2bcf29347ca5f36612a
EricKart <itsary1020@yahoo.com>
2024-05-20 18:30:24 +0530

    Added open weather and aviation stack api

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,43 @@
-// index.js
-
 const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const flightRoutes = require("./routes/flightRoutes");
-
-dotenv.config();
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB", err));
+app.use(express.static("front"));
 
-app.use(express.json());
-app.use("/api", flightRoutes);
+app.get("/api/flights", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `http://api.aviationstack.com/v1/flights?access_key=${process.env.AVIATION_STACK_API_KEY}`
+    );
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+app.get("/api/weather", async (req, res) => {
+  const { city } = req.query;
+  try {
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`
+    );
+    const weatherData = {
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      windSpeed: response.data.wind.speed,
+    };
+    res.json(weatherData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
